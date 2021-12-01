5761ae35c74526534988950a142dfe69487da2ea
costel213 <costelmitrea1@gmail.com>
2021-12-01 18:01:02 +0200

    implement map based services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/costelmitrea/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.costelmitrea.petclinic.services.map;
+
+import com.costelmitrea.petclinic.model.Pet;
+import com.costelmitrea.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long>
+                            implements CrudService<Pet, Long> {
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
