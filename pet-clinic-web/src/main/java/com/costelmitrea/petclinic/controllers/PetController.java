d61d181905b5e319f29d89bcc40a9b52b4c9298c
costel213 <costelmitrea1@gmail.com>
2021-12-01 19:00:49 +0200

    Adding pets index page and controller. Closes #22.

---
+++ pet-clinic-web/src/main/java/com/costelmitrea/petclinic/controllers/PetController.java
@@ -0,0 +1,14 @@
+package com.costelmitrea.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pets")
+public class PetController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String listPets() {
+        return "pets/index";
+    }
+}
