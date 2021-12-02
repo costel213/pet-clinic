995dcae6743f0e9b570fdff1ae7895f4aa0b50cd
costel213 <costelmitrea1@gmail.com>
2021-12-02 11:31:37 +0200

    closes #16

--- pet-clinic-web/src/main/java/com/costelmitrea/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/costelmitrea/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.costelmitrea.petclinic.controllers;
 
+import com.costelmitrea.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listowners() {
+    public String listowners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
