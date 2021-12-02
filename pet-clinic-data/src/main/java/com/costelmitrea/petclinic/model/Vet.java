a5877e20e7a656d166c3bb48af13512712ebc52a
costel213 <costelmitrea1@gmail.com>
2021-12-02 16:01:59 +0200

    closes #28

--- pet-clinic-data/src/main/java/com/costelmitrea/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/costelmitrea/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.costelmitrea.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<VetSpeciality> vetSpecialities;
+
+    public Set<VetSpeciality> getVetSpecialities() {
+        return vetSpecialities;
+    }
+
+    public void setVetSpeciality(Set<VetSpeciality> vetSpecialities) {
+        this.vetSpecialities = vetSpecialities;
+    }
 }
---
+++ pet-clinic-data/src/main/java/com/costelmitrea/petclinic/model/VetSpeciality.java
@@ -0,0 +1,14 @@
+package com.costelmitrea.petclinic.model;
+
+public class VetSpeciality extends BaseEntity{
+
+    private String specialityName;
+
+    public String getSpecialityName() {
+        return specialityName;
+    }
+
+    public void setSpecialityName(String specialityName) {
+        this.specialityName = specialityName;
+    }
+}
