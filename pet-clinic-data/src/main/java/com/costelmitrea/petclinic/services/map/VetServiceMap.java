3c080551795e3d67973f756f5886d74056ee6273
costel213 <costelmitrea1@gmail.com>
2021-12-03 13:49:14 +0200

    update profile to use Spring Data JPA Service. Closes #49.

--- pet-clinic-data/src/main/java/com/costelmitrea/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/costelmitrea/petclinic/services/map/VetServiceMap.java
@@ -4,11 +4,13 @@ import com.costelmitrea.petclinic.model.Vet;
 import com.costelmitrea.petclinic.model.VetSpeciality;
 import com.costelmitrea.petclinic.services.SpecialityService;
 import com.costelmitrea.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetServiceMap extends AbstractMapService<Vet, Long>
                             implements VetService {
 
