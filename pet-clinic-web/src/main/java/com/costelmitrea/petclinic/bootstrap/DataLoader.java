ecf25a8c33336a5ad01672e3eca894552bee9d5a
costel213 <costelmitrea1@gmail.com>
2021-12-03 13:35:03 +0200

    Create Spring Data JPA Service for Visit. Closes #48.

--- pet-clinic-web/src/main/java/com/costelmitrea/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/costelmitrea/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.costelmitrea.petclinic.bootstrap;
 
 import com.costelmitrea.petclinic.model.*;
-import com.costelmitrea.petclinic.services.OwnerService;
-import com.costelmitrea.petclinic.services.PetTypeService;
-import com.costelmitrea.petclinic.services.SpecialityService;
-import com.costelmitrea.petclinic.services.VetService;
+import com.costelmitrea.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,16 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService,
-                    PetTypeService petTypeService, SpecialityService specialityService) {
+                    PetTypeService petTypeService, SpecialityService specialityService,
+                      VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -89,6 +89,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded owners...");
 
