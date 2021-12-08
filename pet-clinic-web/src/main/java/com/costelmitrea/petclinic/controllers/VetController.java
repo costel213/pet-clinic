ebd5834eb3953b523b6773df22e3e12bf6b2750d
costel213 <costelmitrea1@gmail.com>
2021-12-08 16:23:12 +0200

    implemented Vets JSON Endpoint. closes #65

--- pet-clinic-web/src/main/java/com/costelmitrea/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/costelmitrea/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.costelmitrea.petclinic.controllers;
 
+import com.costelmitrea.petclinic.model.Vet;
 import com.costelmitrea.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +26,8 @@ public class VetController {
         return "vets/index";
     }
 
-
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
 }
